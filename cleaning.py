3c5ec12d4989c34b57be460a9e500cfa63ea8206
ValeMTo <valeria.amato@polimi.it>
2025-08-15 17:15:30 +0000

    updating cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -11,7 +11,7 @@ def clean_folders(base_path, keep_list):
             continue
         for item in os.listdir(folder_path):
             item_path = os.path.join(folder_path, item)
-            if os.path.isdir(item_path) and item not in keep_list:
+            if os.path.isdir(item_path) and not any(item in keep for keep in keep_list):
                 shutil.rmtree(item_path)
     if missing_folders:
         raise FileNotFoundError(f"The following required folders were not found: {missing_folders}")
@@ -28,7 +28,7 @@ def clear_snakemake_logs(base_path):
 
 # Example usage:
 pypsa_earth_path = './pypsa_earth'
-folders_to_keep = ['2030_LS']  # Replace with your folder names to keep
+folders_to_keep = ['emission', '10flex']  # Replace with your folder names to keep
 
 clean_folders(pypsa_earth_path, folders_to_keep)
 clear_snakemake_logs(pypsa_earth_path)
